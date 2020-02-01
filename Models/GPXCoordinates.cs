fa99bd8c698bbfd4b2f12e55313c2b73a4533430
enrico.benedos@gmail.com <enrico.benedos@gmail.com>
2020-02-01 14:34:38 +0100

    Added GPXCoordinates class

---
+++ Models/GPXCoordinates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace GPXReaderLib.Models
+{
+    /// <summary>
+    /// Class that define the longitude and latitude properties
+    /// </summary>
+    public class GPXCoordinates
+    {
+        public GPXCoordinates(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
